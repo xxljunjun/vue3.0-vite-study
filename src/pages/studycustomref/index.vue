6b269fcfe889a5c4c0a80e755f45159f5c6530a7
xxljunjun <793117268@qq.com>
2022-03-09 10:50:44 +0800

    vue3的customtoRef

--- src/pages/studycustomref/index.vue
+++ src/pages/studycustomref/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="studycustomref">
-        <input type="text" v-model="keyword">
-        <input type="text" v-model="keyword_1">
-    </div>
+  <div class="studycustomref">
+    <input type="text" v-model="keyword" />
+    {{keyword}}
+  </div>
 </template>
 
 <script>
@@ -10,21 +10,41 @@
     + customRef
         ==>创建一个自定义的ref,并对其依赖项跟踪和更新触发进行显式控制
 */
-import {ref} from 'vue'
+import { ref,customRef } from "vue";
 export default {
-    name:'studycustomref',
-    setup () {
-        // let keyword = ref('hello')  //使用vue提供的ref
-        let keyword = myRef("hello")  //使用程序员自定义的ref
+  name: "studycustomref",
+  setup() {
+    // let keyword = ref("hello"); //使用vue提供的ref
+    let keyword = myRef("hello")  //使用程序员自定义的ref
+
+    function myRef(value, delay = 500) {
+      let timeout;
+      return customRef((track, trigger) => {
         return {
-            keyword
-        }
+          get() {
+            track();
+            console.log("有人读取了")
+            return value;
+          },
+          set(newValue) {
+            console.log("有人修改了",newValue)
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+              value = newValue;
+              trigger();
+            }, delay);
+          },
+        };
+      });
     }
-}
+    return {
+      keyword:myRef('hello')
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.studycustomref{
-
+.studycustomref {
 }
 </style>
